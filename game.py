a2da38128fd0083775cdf8780cd886768d7472d8
Impossible380 <impossible380@tutanota.com>
2024-05-18 09:54:23 +0200

    Délimitation de début et fin de la classe "Game" dans le fichier "game.py"

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from os import name
 from pong_terminal import *
 import time
 
@@ -89,3 +90,44 @@ class Entity:
     def __init__(self, color, position):
         self.color = color
         self.position = list(position)
+
+
+if name == "__main__":
+    """   Début Game   """
+    game = Game(players)
+
+    x_move_ball = 3
+    y_move_ball = 1
+
+    time.sleep(1)
+    # Routine d'envoi
+    while is_playing:
+        # Mouvement de la balle
+        x_move_ball, y_move_ball = game.move_ball(x_move_ball, y_move_ball)
+        
+        entities = [game.ball, game.racket_1, game.racket_2]
+
+        for player in game.players:
+            if player["has_moved"]:
+                game.move_racket(player["id"], player["has_moved"] * 2)
+                player["has_moved"] = 0
+
+            # Envoi de la position de la balle et des raquettes
+            for entity in entities:
+                send_string(player["socket"], entity.color)
+                send_position(player["socket"], *entity.position)
+
+            # Envoi des scores
+            send_number(player["socket"], game.players[0]["score"])
+            send_number(player["socket"], game.players[1]["score"])
+        
+        if game.players[0]["score"] >= 3 or game.players[1]["score"] >= 3:
+            break
+        
+    is_playing = False
+
+    print(is_playing)
+
+    for player in players:
+        player["thread"].join()
+    """   Fin Game   """
